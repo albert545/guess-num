71fe6ff7a717a766c08b93bd62c8822ee9a35d13
Albert Liao <pulieden@gmail.com>
2018-03-02 16:49:15 +0800

    base verison

---
+++ guess.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  2 16:15:25 2018
+
+@author: albert-w10
+"""
+
+import random
+
+r = random.randint(1,100)
+
+while True:
+    num = input('請猜數字:')
+    num = int(num)
+    if num == r:
+        print('你猜對了')
+        break
+    elif num > r:
+        print('比答案大')
+    elif num < r:
+        print('比答案小')
+    
+        
+    
+        
