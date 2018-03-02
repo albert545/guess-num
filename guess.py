b3d740301d5e42f4b6a262441403f21f661767c5
Albert Liao <pulieden@gmail.com>
2018-03-02 16:59:31 +0800

    計算猜了幾次

--- guess.py
+++ guess.py
@@ -8,17 +8,21 @@ Created on Fri Mar  2 16:15:25 2018
 import random
 
 r = random.randint(1,100)
+t = 0
 
 while True:
+    t = t + 1
     num = input('請猜數字:')
     num = int(num)
     if num == r:
         print('你猜對了')
+        print('你總共猜了:',t,'次')
         break
     elif num > r:
         print('比答案大')
     elif num < r:
         print('比答案小')
+    print('這是您猜的第',t,'次')    
     
         
     
